37e93152ecab505845878dd154a83f405b786890
laskor <al.moksud@gmail.com>
2022-03-17 02:27:03 -0400

    spa

---
+++ spa.js
@@ -0,0 +1,18 @@
+
+// spa (single page application)
+
+/*
+
+A SPA application is a single page that continuously interacts with the user by dynamically rewriting 
+the current page rather than loading entire new pages from a server.
+    Trello, Facebook, Gmail, and Twitter are a few single page app examples.
+
+*/
+
+//  Single Page Application Pros:
+
+/*  1.Quick Loading Time
+    2.Seamless User Experience
+    3.Ease in Building Feature-rich Apps
+    4.Uses Less Bandwidth 
+*/
