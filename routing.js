6739036464653e3f7bfedcddfbdf5438d8d87e8d
laskor <al.moksud@gmail.com>
2022-03-18 19:57:08 -0400

    routing

---
+++ routing.js
@@ -0,0 +1,5 @@
+
+
+/* React Router is a standard library for routing in React. 
+It enables the navigation among views of various components in a React Application,
+allows changing the browser URL, and keeps the UI in sync with the URL. */
