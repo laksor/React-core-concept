891019f7db0f9df07ee20d28d33788f5f5a83272
laskor <al.moksud@gmail.com>
2022-03-18 19:59:33 -0400

    defination

--- routing.js
+++ routing.js
@@ -2,4 +2,22 @@
 
 /* React Router is a standard library for routing in React. 
 It enables the navigation among views of various components in a React Application,
-allows changing the browser URL, and keeps the UI in sync with the URL. */
+allows changing the browser URL, and keeps the UI in sync with the URL. 
+
+Adding React Router Components: The main Components of React Router are:
+
+BrowserRouter: BrowserRouter is a router implementation that uses the HTML5 history
+API(pushState, replaceState and the popstate event) to keep your UI in sync with the URL.
+It is the parent component that is used to store all of the other components.
+
+Routes: It’s a new component introduced in the v6 and a upgrade of the component.
+The main advantages of Routes over Switch are:
+
+Relative s and s
+Routes are chosen based on the best match instead of being traversed in order.
+
+Route: Route is the conditionally shown component that renders some UI 
+when its path matches the current URL.
+
+Link: Link component is used to create links to different routes and implement navigation around the application. It works like HTML anchor tag.
+*/
