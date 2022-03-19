fb658325a578844c6d2d0fa48aa3a6af0443186c
laskor <al.moksud@gmail.com>
2022-03-18 20:24:20 -0400

    countries api

---
+++ js/countries.js
@@ -0,0 +1,21 @@
+const loadCountries = () => {
+    fetch('https://restcountries.com/v3.1/all')
+        .then (res => res.json())
+        .then(data => displayCountries(data))
+}
+
+
+const displayCountries = countries => {
+    const allCountries = countries.map(country => getCountryHtml(country))
+    const container = document.getElementById('main');
+    container.innerHTML = allCountries.join(' ');
+}
+
+const getCountryHtml = country => {
+    return `
+    <div>
+        <h2>${country.name.common}</h2>
+    </div>
+    `
+}
+loadCountries();
